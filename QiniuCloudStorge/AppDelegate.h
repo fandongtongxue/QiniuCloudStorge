842d57f00d24466a00cf6d4779c55bc3c9870f3b
范东 <fandongme@fandongdeMacBook-Pro.local>
2016-09-23 17:35:02 +0800

    添加工程文件

---
+++ QiniuCloudStorge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QiniuCloudStorge
+//
+//  Created by 范东 on 16/9/23.
+//  Copyright © 2016年 范东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QiniuCloudStorge/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QiniuCloudStorge
+//
+//  Created by 范东 on 16/9/23.
+//  Copyright © 2016年 范东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
